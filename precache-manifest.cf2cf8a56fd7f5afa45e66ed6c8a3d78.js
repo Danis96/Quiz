5f98f348046e794962895c14ee4f03eab633a702
Danis96 <danis.preldzic@gmail.com>
2019-11-16 11:04:29 +0100

    Updates

---
+++ precache-manifest.cf2cf8a56fd7f5afa45e66ed6c8a3d78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd783870470ffade68114d3b596493b6",
+    "url": "/Quiz/index.html"
+  },
+  {
+    "revision": "5b9a94e5f253d1cc7221",
+    "url": "/Quiz/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "12f1f6f2a5e12ca9ca11",
+    "url": "/Quiz/static/js/2.77170ad2.chunk.js"
+  },
+  {
+    "revision": "5b9a94e5f253d1cc7221",
+    "url": "/Quiz/static/js/main.40877e1e.chunk.js"
+  },
+  {
+    "revision": "e295ff50d7170b66b0b1",
+    "url": "/Quiz/static/js/runtime-main.e206d4fe.js"
+  }
+]);
